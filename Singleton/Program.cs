b548d4eec0dd792d1ed9d5781b3fa1c650f3998d
Hamdi Çatal <hamdicatal@users.noreply.github.com>
2018-12-23 22:30:07 +0300

    Basit seviyede Singleton Design Pattern implementasyonu yapıldı

--- Singleton/Program.cs
+++ Singleton/Program.cs
@@ -10,6 +10,36 @@ namespace Singleton
     {
         static void Main(string[] args)
         {
+            var customerManager = CustomerManager.CreateSingleton();
+            customerManager.Save();
+        }
+
+        class CustomerManager
+        {
+            private static CustomerManager _customerManager;
+
+            private CustomerManager()
+            {
+
+            }
+
+            // CustomerManager nesnesinin örneğinden sadece bir kez static olarak üretilmesini sağlayan metodumuz.
+            public static CustomerManager CreateSingleton()
+            {
+                // CustomerManager daha önce oluşturulmamışsa ilk ve son örneği oluşturur.
+                if (_customerManager == null)
+                {
+                    _customerManager = new CustomerManager();
+                }
+
+                return _customerManager;
+            }
+
+            public void Save()
+            {
+                Console.WriteLine("Saved!");
+                Console.ReadLine();
+            }
         }
     }
 }
