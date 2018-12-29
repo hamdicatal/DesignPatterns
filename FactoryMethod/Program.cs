f18df3478c9c999d04b5e434314b616174e52ddc
Hamdi Çatal <hamdicatal@users.noreply.github.com>
2018-12-29 11:40:07 +0300

    Factory metoduyla alakalı düzenlemeler ve refactoring yapıldı

--- FactoryMethod/Program.cs
+++ FactoryMethod/Program.cs
@@ -10,23 +10,36 @@ namespace FactoryMethod
     {
         static void Main(string[] args)
         {
-            CustomerManager manager = new CustomerManager();
+            //İhtiyaca göre kullanılacak factory değiştirilebilir. Bu değişiklik kodda hiçbir yeri etkilemez.
+            //CustomerManager manager = new CustomerManager(new LoggerFactory2());
+            CustomerManager manager = new CustomerManager(new LoggerFactory());
             manager.Save();
             Console.ReadLine();
         }
     }
 
+    //Class oluştururken bir soyut class veya interface mutlaka yazılmalı. Yeni bir factory oluşturmak istediğimizde bağımlılıklardan dolayı sorun yaşayabiliriz.
     public class LoggerFactory:ILoggerFactory
     {
         public ILogger CreateLogger()
         {
+            //Duruma göre farklı log'lama işlemleri yapılabilir.
+            return new DemoLogger();
+        }
+    }
+    public class LoggerFactory2 : ILoggerFactory
+    {
+        public ILogger CreateLogger()
+        {
+            //Duruma göre farklı log'lama işlemleri yapılabilir.
             return new DemoLogger();
         }
     }
 
     public interface ILoggerFactory
     {
-
+        //Bütün factory'lerde implemente edilmek zorunda.
+        ILogger CreateLogger();
     }
 
     public interface ILogger
@@ -40,18 +53,32 @@ namespace FactoryMethod
         {
             Console.WriteLine("Logged with DemoLogger!");
         }
+    }
 
+    public class DemoLogger2: ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged with DemoLogger2!");
+        }
     }
 
     public class CustomerManager
     {
+        //Herhangi bir factory'e bağımlı kalmadan, injection işlemi yapılabilir.
+        ILoggerFactory _loggerFactory;
+
+        public CustomerManager(ILoggerFactory loggerFactory)
+        {
+            _loggerFactory = loggerFactory;
+        }
+
         public void Save()
         {
             Console.WriteLine("Saved!");
-            //ILogger logger = new DemoLogger(); //DemoLogger class'ına bağımlı olduk. Yanlış kullanım. "new" yaparken dikkat edilmeli. Bunun yerine factory ile üretilebilir.
-            ILogger logger = new LoggerFactory().CreateLogger(); //Factory tasarım kalıbına uygun ideal kullanım.
+            //ILogger logger = new DemoLogger(); //DemoLogger class'ına bağımlı olduk. Yanlış kullanım. Bunun yerine factory metodu ile üretilebilir.
+            ILogger logger = _loggerFactory.CreateLogger(); //Factory tasarım kalıbına uygun ideal kullanım.
             logger.Log();
-
         }
     }
 }
