dfac1180ea5c3f6c7ddf4fcaa235daaf54a7b0eb
Hamdi Çatal <hamdicatal@users.noreply.github.com>
2018-12-29 12:14:18 +0300

    Abstract Factory'ler oluşturuldu. Refactoring yapıldı.

--- AbstractFactory/Program.cs
+++ AbstractFactory/Program.cs
@@ -10,21 +10,27 @@ namespace AbstractFactory
     {
         static void Main(string[] args)
         {
-
+            //Dependency injection containerların kullanılması daha uygun olur.
+            //ProductManager productManager = new ProductManager(new Factory1());
+            ProductManager productManager = new ProductManager(new Factory2());
+            productManager.GetAll();
+            Console.ReadLine();
         }
     }
 
     //Logging ve Caching olmak üzere ihtiyaçlarımıza uygun iki temel soyut nesne oluşturduk. Bu iki nesneyi inherit eden, sayısız nesne oluşturabiliriz.
 
+    //Duruma göre Logging ve Caching teknikleri arasında seçim yapacak factory'e ihtiyacımız var. Bu da bizim "Abstract Factory" nesnemiz olacak.
+
     //Yeni bir loglama yöntemi eklediğimizde sistem bozulmayacak. 
     public abstract class Logging
     {
-        public abstract void Log(string message);
+        public abstract void Log();
     }
 
     public class DemoLogger : Logging
     {
-        public override void Log(string message)
+        public override void Log()
         {
             Console.WriteLine("Logged with DemoLogger!");
         }
@@ -32,7 +38,7 @@ namespace AbstractFactory
 
     public class OtherLogger : Logging
     {
-        public override void Log(string message)
+        public override void Log()
         {
             Console.WriteLine("Logged with OtherLogger!");
         }
@@ -41,12 +47,12 @@ namespace AbstractFactory
     //Yeni bir caching yöntemi eklediğimizde sistem bozulmayacak. 
     public abstract class Caching
     {
-        public abstract void Cache(string data);
+        public abstract void Cache();
     }
 
     public class DemoCache : Caching
     {
-        public override void Cache(string data)
+        public override void Cache()
         {
             Console.WriteLine("Cached with DemoCache!");
         }
@@ -54,10 +60,66 @@ namespace AbstractFactory
 
     public class OtherCache : Caching
     {
-        public override void Cache(string data)
+        public override void Cache()
         {
             Console.WriteLine("Cached with OtherCache!");
         }
     }
 
+    //Abstract Factory. Ayrıca bu soyut class'dan yeni factory'ler üretilebilir.
+    public abstract class CrossCuttingConcernsFactory
+    {
+        public abstract Logging CreateLogger();
+        public abstract Caching CreateCaching();
+    }
+
+    public class Factory1 : CrossCuttingConcernsFactory //inheritance
+    {
+        public override Caching CreateCaching()
+        {
+            return new DemoCache();
+        }
+
+        public override Logging CreateLogger()
+        {
+            return new DemoLogger();
+        }
+    }
+
+    //İş süreçlerine ve ihtiyaçlara göre yeni factory'ler üretilebilir. 
+    public class Factory2 : CrossCuttingConcernsFactory
+    {
+        public override Caching CreateCaching()
+        {
+            return new OtherCache();
+        }
+
+        public override Logging CreateLogger()
+        {
+            return new OtherLogger();
+        }
+    }
+
+    //Bu factory'leri kullanan bir client veya iş katmanı gerekir.
+    public class ProductManager
+    {
+        //Dependency injection işlemi;
+        private CrossCuttingConcernsFactory _crossCuttingConcernsFactory;
+        private Logging _logging;
+        private Caching _caching;
+
+        public ProductManager(CrossCuttingConcernsFactory crossCuttingConcernsFactory)
+        {
+            _crossCuttingConcernsFactory = crossCuttingConcernsFactory;
+            _logging = crossCuttingConcernsFactory.CreateLogger();
+            _caching = crossCuttingConcernsFactory.CreateCaching();
+        }
+
+        public void GetAll()
+        {
+            _logging.Log();
+            _caching.Cache();
+            Console.WriteLine("Products listed!");
+        }
+    }
 }
